20593801c445b091664caae452b8beb4c22a2fd0
Anton Makoveev <makoveev90@gmail.com>
2026-01-19 18:24:02 +0100

    [WiP]: add a pdf viewer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,29 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+'use client'
+
+
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
+
+const AppPdfViewer = dynamic(() => import('@/app/ui/pdf-viewer/pdfviewer'), {
+  ssr: false,
+});
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex h-screen flex-col">
       <header className="bg-blue-600 text-white">
         <div className="mx-auto px-4 py-4 text-2xl font-semibold">
           Claim Assistant
         </div>
       </header>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/4 md:px-20">
+      <div className="ml-4 mr-4 flex flex-1 min-h-0 flex-col gap-4 py-4 md:flex-row">
+        <div className="flex min-h-0 flex-1 items-stretch md:w-2/4 md:px-0">
+          <div className="flex min-h-0 w-full flex-1 flex-col rounded-lg bg-gray-50 p-4">
+            <AppPdfViewer />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 md:w-2/4 md:px-10">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -25,9 +37,6 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-2/4 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
         </div>
       </div>
     </main>
